e676f42a3521307b95dcda3bb373800e74ada008
Saswater <saswata.majumder@cern.ch>
2025-06-26 12:06:36 -0400

    First push - modifying requirements.txt (will test later)

---
+++ setup_venv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python -m venv .venv
+source .venv/bin/activate
+
+pip install -r reqs-short.txt
+pip install torch==2.2.1+cu118 torchvision==0.17.1+cu118 torchaudio==2.2.1 --extra-index-url https://download.pytorch.org/whl/cu118
+
+pip install -r reqs-long.txt
