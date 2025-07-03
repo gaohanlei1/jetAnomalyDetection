6b07794849f19dcc3eb4df9c839c37eb3519060a
Saswater <saswata.majumder@cern.ch>
2025-07-03 16:36:51 +0200

    added symlinker

--- setup_data_symlinks.sh
+++ setup_data_symlinks.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-
 # can move these setup files to ./setup/?
-ln -s /isilon/export/home/mstamenk/jet-anomaly-summer25/downloads/jekrupa/v2_2/2017/QCD/QCD_Pt_1000to1400_TuneCP5_13TeV_pythia8/QCD_Pt_1000to1400/211109_133059/0000 ./data/raw/QCD
 
-ln -s /isilon/export/home/mstamenk/jet-anomaly-summer25/downloads/jekrupa/v2_2/2017/WJetsToQQ/WJetsToQQ_HT-400to600_TuneCP5_13TeV-madgraphMLM-pythia8/WJetsToQQ_HT-400to600/211108_171900/0000 ./data/raw/WJet
+qcd_dir = "/isilon/export/home/mstamenk/jet-anomaly-summer25/downloads/jekrupa/v2_2/2017/QCD/QCD_Pt_1000to1400_TuneCP5_13TeV_pythia8/QCD_Pt_1000to1400/211109_133059/0000"
+wjet_dir = "/isilon/export/home/mstamenk/jet-anomaly-summer25/downloads/jekrupa/v2_2/2017/WJetsToQQ/WJetsToQQ_HT-400to600_TuneCP5_13TeV-madgraphMLM-pythia8/WJetsToQQ_HT-400to600/211108_171900/0000"
+
+for filepath in $qcd_dir/*.root; do 
+    ln -s "$filepath" "./data/raw/QCD/${filepath##*/}"
+done
+
+for filepath in $wjet_dir/*.root; do 
+    ln -s "$filepath" "./data/raw/QCD/${filepath##*/}"
+done
+
+# ln -s  ./data/raw/QCD
+
+# ln -s  ./data/raw/WJet
--- setup_venv.sh
+++ setup_venv.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # can move these setup files to ./setup/?
-python -m venv .venv
+python3.9 -m venv .venv
 source start_venv.sh
 #.venv/bin/python -m pip install --upgrade pip
 
-pip install -r reqs-short.txt
-pip install torch==2.2.1+cu118 torchvision==0.17.1+cu118 torchaudio==2.2.1 --extra-index-url https://download.pytorch.org/whl/cu118
+#pip install -r reqs-short.txt
+#pip install torch==2.2.1+cu118 torchvision==0.17.1+cu118 torchaudio==2.2.1 --extra-index-url https://download.pytorch.org/whl/cu118
 
-pip install -r reqs-long.txt
+pip install --ignore-installed torch-scatter -f https://data.pyg.org/whl/torch-2.2.1+cu121.html
+pip install --ignore-installed  torch-sparse -f https://data.pyg.org/whl/torch-2.2.1+cu121.html
+pip install --ignore-installed  torch-cluster -f https://data.pyg.org/whl/torch-2.2.1+cu121.html
+pip install --ignore-installed  torch-geometric -f https://data.pyg.org/whl/torch-2.2.1+cu121.html
+
+pip install -r ../reqs-trans.txt
