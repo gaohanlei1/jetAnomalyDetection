84c7861afe9c0526b46519a249286631d628b488
Saswater <saswata.majumder@cern.ch>
2025-07-02 21:25:17 -0400

    changes to requirements, to try locally

--- setup_venv.sh
+++ setup_venv.sh
@@ -3,6 +3,7 @@
 # can move these setup files to ./setup/?
 python -m venv .venv
 source start_venv.sh
+#.venv/bin/python -m pip install --upgrade pip
 
 pip install -r reqs-short.txt
 pip install torch==2.2.1+cu118 torchvision==0.17.1+cu118 torchaudio==2.2.1 --extra-index-url https://download.pytorch.org/whl/cu118
